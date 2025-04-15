519ae6ebd8b277c5ec31ba0ad0cdfc916d730bee
Lucas <lucasnrgaard@gmail.com>
2025-04-15 17:09:17 +0200

    chore: debug tests

--- playground.ts
+++ playground.ts
@@ -5,3 +5,4 @@ console.log(compare("1.0.0", "1.0.1", "<")); // true
 console.log(compare("1.0.0", "1.0.1", ">")); // false
 console.log(compare("1.0.0", "1.0.0", "<=")); // true
 console.log(compare("1.0.0", "1.0.0", ">=")); // true
+console.log(compare("1.2.-3a", "1.0.0", ">")); // false
